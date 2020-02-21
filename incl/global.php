9e46b6fc8a9315eb9e6406bf853ddc1e12b33997
CY <cywhiz@gmail.com>
2020-02-22 01:54:28 +0800

    New update

--- incl/global.php
+++ incl/global.php
@@ -1,9 +1,11 @@
-<!-- ========== Database login info ========== -->
-
 <?php
-	$host = 'localhost';
-	$user = 'root';
-	$pass = '';
-	$name = 'ezcart';
-	$db = &new MySQL($host,$user,$pass,$name);
+// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$url = parse_url('mysql://bfe344d917d9e6:4212ed92@us-cdbr-iron-east-03.cleardb.net/heroku_5481d3e276397ed?reconnect=true');
+
+$host = $url["host"];
+$dbUser = $url["user"];
+$dbPass = $url["pass"];
+$dbName = substr($url["path"], 1);
+
+$db = new MySQL($host, $dbUser, $dbPass, $dbName);
 ?>
--- search.php
+++ search.php
@@ -1,6 +1,6 @@
 <!-- Search product by name or keyword -->
 
-<form name="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get"> 
+<form name="form" action="" method="get"> 
 	<input name="inpname" type="text" id="inpname" size="50" value="Type in product name or keyword" name="s" id="s" onblur="if(this.value=='') this.value='Type in product name or keyword';" onfocus="if(this.value=='Type in product name or keyword') this.value='';" > 
 	<input type="submit" name="search" value="Search"> 
 </form>
